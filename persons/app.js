066b45b7c5adb0b0a9a090a9d7cddd4ac3bef242
luismoramedina <luismoramedina@gmail.com>
2016-07-07 20:41:57 +0200

    add security header handling

--- persons/app.js
+++ persons/app.js
@@ -5,6 +5,15 @@ var express = require('express')();
 
 express.use(bodyParser.json());
 
+//security middleware
+express.use(function (req, res, next) {
+   if (!req.get('Authorization')) {
+      res.status(401).send('Authentication required');
+      return;
+   }
+   next();
+});
+
 express.get('/api/persons/:personid', function(req, res, next) {
     res.json({
         dni: req.params.personid,
