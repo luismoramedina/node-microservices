c7a16eebb5c481f7cd86aa025a5d3f4627453e7b
luismoramedina <luismoramedina@gmail.com>
2016-07-10 20:44:01 +0200

    better error handling, request count

--- composite/app.js
+++ composite/app.js
@@ -2,13 +2,22 @@ var express = require('express')();
 var bodyParser = require('body-parser');
 var client = require('./http-client');
 
+
 express.use(bodyParser.json());
 
+var requestCount = 0;
+
+express.use(function(req, res, next) {
+   requestCount++;
+   console.log('requestCount', requestCount);
+   next();
+});
+
+
 function compose(callback, err_callback) {
-   client.persons(function(personsdata, err_p) {
-      client.accounts(function(accountsdata, err_a) {
-         if (err_a || err_p 
-            || !accountsdata || !personsdata) {
+   client.persons(function(err_p, personsdata) {
+      client.accounts(function(err_a, accountsdata) {
+         if (err_a || err_p) {
             err_callback(err_a || err_p);
          } else {
             var persons = JSON.parse(personsdata);
@@ -25,7 +34,7 @@ express.get('/api/clients/:id', function(req, res, next) {
       console.log('send response!');
       res.send(data);
    }, function(err) {
-      console.log(err);
+      console.log('err', err);
       res.status(500).send('error on services');
    });
 });
--- composite/http-client.js
+++ composite/http-client.js
@@ -7,23 +7,26 @@ var options = {
 };
 
 exports.persons = function(callback) {
-  options.url = 'http://localhost:8081/api/persons/1';
+  options.url = 'http://localhost:8081/api/persoAns/1';
   request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(body);
-    } else {
-      callback(null, error);
-    }
+    handleResponse(error, response, body, callback);
   });
 }
 
 exports.accounts = function(callback) {
   options.url = 'http://localhost:8082/api/accounts/1';
   request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(body);
-    } else {
-      callback(null, error);
-    }
+    handleResponse(error, response, body, callback);
   });
+}
+
+function handleResponse(error, response, body, callback) {
+  if (!error && response.statusCode == 200) {
+    callback(null, body);
+  } else {
+    if (!error) {
+      error = new Error({'statusCode' : response.statusCode});
+    }
+    callback(error);
+  }
 }
