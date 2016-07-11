519e31a5f7fa3ecfb70b9ea2e2acdb1981ce4ef0
luismoramedina <luismoramedina@gmail.com>
2016-07-11 12:15:35 +0200

    changes accounts url for get

--- accounts/app.js
+++ accounts/app.js
@@ -3,7 +3,10 @@ var bodyParser = require('body-parser');
 
 var express = require('express')();
 
-express.get('/api/accounts/:personid', function(req, res, next) {
+express.get('/api/accounts', function(req, res) {
+   console.log('gs-auth-token: ', req.get('GS-AUTH-TOKEN'));
+   console.log('dni: ', req.query.dni);
+
     res.json([{
         accountId: 1,
         dni: 1,
